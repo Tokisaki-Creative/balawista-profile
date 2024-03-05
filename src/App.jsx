40534330ca00db090538425c29849d1d8f66b6e8
BagusAri-17 <bagusari.widhi@gmail.com>
2024-03-05 09:56:17 +0800

    add helmet in detail views

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Footer from "./components/layout-components/Footer"
 import DetailArticleTwo from "./views/article-views/DetailArticleTwo";
 import DetailDestinationOne from "./views/destination-views/DetailDestinationOne";
 import DetailDestinationTwo from "./views/destination-views/DetailDestinationTwo";
+import { Helmet } from "react-helmet";
 
 function App() {
 
@@ -20,6 +21,11 @@ function App() {
             <Route path="/artikel" element={<Article />} />
             <Route path="/detail-artikel/1" element={
               <>
+                <Helmet>
+                  <meta charSet="utf-8" />
+                  <title>Artikel 1 | Travel Tips</title>
+                  <link rel="canonical" href="http://mysite.com/example" />
+                </Helmet>
                 <Header />
                 <DetailArticleOne />
                 <Footer />
@@ -27,6 +33,11 @@ function App() {
             } />
             <Route path="/detail-artikel/2" element={
               <>
+                <Helmet>
+                  <meta charSet="utf-8" />
+                  <title>Artikel 2 | Travel Tips</title>
+                  <link rel="canonical" href="http://mysite.com/example" />
+                </Helmet>
                 <Header />
                 <DetailArticleTwo />
                 <Footer />
@@ -34,6 +45,11 @@ function App() {
             } />
             <Route path="/detail-destinasi/1" element={
               <>
+                <Helmet>
+                  <meta charSet="utf-8" />
+                  <title>Destinasi | Tanah Lot</title>
+                  <link rel="canonical" href="http://mysite.com/example" />
+                </Helmet>
                 <Header />
                 <DetailDestinationOne />
                 <Footer />
@@ -41,6 +57,11 @@ function App() {
             } />
             <Route path="/detail-destinasi/2" element={
               <>
+                <Helmet>
+                  <meta charSet="utf-8" />
+                  <title>Destinasi | Babi Guling</title>
+                  <link rel="canonical" href="http://mysite.com/example" />
+                </Helmet>
                 <Header />
                 <DetailDestinationTwo />
                 <Footer />
